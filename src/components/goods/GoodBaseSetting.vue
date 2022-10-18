771c1d74593da3ebd9d252b251185f79c350e123
wjl <1541558237@qq.com>
2022-10-18 15:53:18 +0800

    new

---
+++ src/components/goods/GoodBaseSetting.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "GoodBaseSetting"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/goods/GoodPriceSetting.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "GoodPriceSetting"
+}
+</script>
+
+<style scoped>
+
+</style>
