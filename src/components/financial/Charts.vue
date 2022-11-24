8884cbe832c5665e93965be058e7f6d8e916daee
wjl <1541558237@qq.com>
2022-11-24 09:49:07 +0800

    manger/zhexian

---
+++ src/components/financial/Charts.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Charts"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/financial/DataCom.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "DataCom"
+}
+</script>
+
+<style scoped>
+
+</style>
